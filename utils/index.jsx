a0c89a3326a1a38de24208e4e83bc4431b91d092
RuslikProg <rkvacuk@gmail.com>
2022-12-07 14:16:24 +0200

    changes

--- utils/index.jsx
+++ utils/index.jsx
@@ -5,7 +5,7 @@ import API_URL from '../constants/index';
 export async function loadPosts() {
   // Call an external API endpoint to get posts
   const res = await fetch(API_URL)
-  const data = await res.json()
+  const data = await res?.json()
 
   return data;
 }
